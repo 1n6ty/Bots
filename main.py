84b34d10855ae274c4deb0c927582522bce3277f
1n6ty <artyom.inety@gmail.com>
2023-09-14 19:46:32 +0700

    mem_bot done

---
+++ main.py
@@ -0,0 +1,75 @@
+import telebot
+from telebot import types
+
+import datetime
+import json
+
+file_path = './table.json'
+with open(file_path, 'r') as f:
+    table = json.loads(f.read())
+
+token = ''
+bot = telebot.TeleBot(token)
+
+chat_id = ''
+
+def add2table(text: str, current_ind: int):
+    global table
+
+    if current_ind == 0:
+        time_d = datetime.timedelta(days=1)
+    elif current_ind == 1:
+        time_d = datetime.timedelta(days=14)
+    elif current_ind == 2:
+        time_d = datetime.timedelta(days=60)
+    else:
+        return
+    
+    day_rep = (datetime.datetime.now().date() + time_d).strftime("%m/%d/%y")
+
+    if day_rep in table:
+        table[day_rep].append([text, current_ind + 1])
+    else:
+        table[day_rep] = [[text, current_ind + 1]]
+    
+
+@bot.message_handler(commands=['start'])
+def start_message(msg):
+    bot.send_message(msg.chat.id, f'Chat id - {msg.chat.id}')
+
+@bot.message_handler(commands=['button'])
+def get_table_btn(message):
+    markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn=types.KeyboardButton("Список на сегодня")
+    markup.add(btn)
+
+    bot.send_message(message.chat.id, reply_markup=markup)
+
+@bot.message_handler(content_types='text')
+def message_reply(msg):
+    if not (str(msg.chat.id) == chat_id):
+        return
+    
+    if msg.text == 'Список на сегодня':
+        day_rep = datetime.datetime.now().date()
+
+        if day_rep.strftime("%m/%d/%y") in table:
+            bot.send_message(msg.chat.id, f'{day_rep.strftime("%m/%d/%y")}\n' + "\n".join([i[0] for i in table[day_rep.strftime("%m/%d/%y")]]))
+        else:
+            bot.send_message(msg.chat.id, 'Ничего на сегодня нет')
+        
+        miss_dates = [i for i in table if datetime.datetime.strptime(i, '%m/%d/%y').date() < day_rep]
+        
+        for d in miss_dates:
+            for i in table[d]:
+                add2table(*i)
+
+        for d in miss_dates:
+            del table[d]
+    else:
+        add2table(msg.text, 0)
+    
+    with open(file_path, 'w') as f:
+        f.write(json.dumps(table))
+
+bot.infinity_polling()
